9f37edcc97b60679f432a07d5c5f8f9997a2a557
gerberth92 <gerberthcontreras@gmail.com>
2023-12-17 15:56:29 -0500

    update 2

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel:
         self.updated_at = self.save()
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(type(self).__name__, 
-                                      self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(type(self)
+                                      .__name__, self.id, self.__dict__))
 
     def save(self):
         self.updated_at = datetime.datetime.now()
@@ -23,4 +23,3 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return (new_dict)
-    
