47aaf38ce463a6a28ae48070367f515826d77416
gerberth92 <gerberthcontreras@gmail.com>
2023-12-21 16:23:38 -0500

    update 2

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,11 @@ import models
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
-            for key, value in kwargs.items():
+            for key, value in kwargs.copy().items():
                 if key == "__class__":
                     del kwargs[key]
 
-                if key == ["created_at", "updated_at"]:
+                if key in ["created_at", "updated_at"]:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value) 
 
